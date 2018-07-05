9629e3f96ee3a04a61a62d5dd9a3cc9912c87e55
József Tar <szurilo@gmail.com>
2018-07-05 11:20:05 +0200

    fine

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(function() {
     var canvas=document.getElementById('MyCanvas');
     var context=canvas.getContext('2d');
-
-     for (j=0;j<6;j++){
-        context.fillRect(j*40,40,38,38);
-    }
-
+    var r = 199;
+    var b = 120;
+    for (i=0;i<6;i++){
+      
+      for (j=0;j<6;j++){
+        context.fillStyle='rgb(' + r + ',79,'+ b +')';
+        r-=7;
+        context.fillRect(j*40+40,40+i*40,38,38);
+      }
+      b+=15;
+  }
   });
